e6135510b7f2409cf0ad2873381878b174fba285
Abhi-1U <62772223+Abhi-1U@users.noreply.github.com>
2022-08-26 16:56:34 +0530

    initial commit

---
+++ bookdown-equations.lua
@@ -0,0 +1,56 @@
+--- bookdown-equation.lua - A Pandoc Lua filter for Converting LaTeX equation
+--- labels to Bookdown Style equation labels in MathJax environment.
+--- pandoc generated sample : $$\label{eq:EB} LE = R_n - G - H$$
+--- filter generated equation : $$\label{eq:EB} LE = R_n - G - H (\#eq:EB)$$
+--- Copyright: © 2021-2022 Abhishek Ulayil
+--- License: MIT – see LICENSE for details
+
+-- Makes sure users know if their pandoc version is too old for this
+-- filter.
+PANDOC_VERSION:must_be_at_least '2.9'
+
+
+-- table to store all equation labels
+equation_labels = {}
+
+--[[
+Converts LaTeX label to bookdown label format in the Math element
+--]]
+function Math(el)
+    if el.text:match('label') then
+        local text = pandoc.utils.stringify(el.text)
+        s, e, l =string.find(text,"\\label{(.-)}")
+        table.insert(equation_labels,l)
+        -- Bookdown does not support period in equations hence substituting them as hyphen
+        l = string.gsub(l, "%.", "-")
+        el.text = text .. [[ (\#]] .. l .. [[)]]
+    else
+        --pass
+    end
+    return(el)
+end
+
+--[[
+Converts Links to equations to modified format
+--]]
+function Link(el)
+    local is_bkdwn = false
+    for _,label in pairs(equation_labels) do
+        if ("#"..label) == el.target then
+            local link_text = el.target
+            -- Bookdown does not support period in equations hence substituting them as hyphen
+            link_text = string.gsub(link_text, "%.", "-")
+            label = string.gsub(label, "%.", "-")
+            el.target = link_text
+            --bookdown type reference constructor
+            bkdwn = [[\@ref(]] .. label .. [[)]]
+            is_bkdwn = true
+            break
+        end
+    end
+    if is_bkdwn then
+        return pandoc.RawInline('markdown', bkdwn)
+    else
+        return(el)
+    end
+end
---
+++ test/expected.md
@@ -0,0 +1,8 @@
+---
+author:
+- Abhi-1U
+title: Using bookdown equations Lua filter
+---
+
+$$\label{eq:emc.2}
+    e = m c^2 (\#eq:emc-2)$$ This is a new equation \@ref(eq:emc-2)
